562afdcb4a8e1aaeed284466248bc6f124d8554a
Malleck <malleck.1206@gmail.com>
2023-08-30 13:12:59 -0500

    Shopping Cart Menu Added

--- main.js
+++ main.js
@@ -2,14 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const asideCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuBurguer.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleAsideCarrito);
 
 function toggleDesktopMenu() {
+  const isAsideCarritoClosed = asideCarrito.classList.contains('inactive');
+
+  if(!isAsideCarritoClosed){
+    asideCarrito.classList.toggle('inactive');
+  }
+
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  const isAsideCarritoClosed = asideCarrito.classList.contains('inactive');
+
+  if(!isAsideCarritoClosed){
+    asideCarrito.classList.toggle('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleAsideCarrito() {
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if(!isDesktopMenuClosed){
+    desktopMenu.classList.toggle('inactive');
+  }
+
+  if(!isMobileMenuClosed){
+    mobileMenu.classList.toggle('inactive');
+  }
+
+  asideCarrito.classList.toggle('inactive');
 }
